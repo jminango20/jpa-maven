62f5836b0e8727ede391df52f05b556afe126b91
jminango20 <jcarlosminango@gmail.com>
2021-03-08 13:37:41 -0300

    Passo 3: Removendo da DB um objeto - ERROR proposital

--- src/application/src/main/java/application/Program.java
+++ src/application/src/main/java/application/Program.java
@@ -9,16 +9,16 @@ import dominio.Pessoa;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquin Balhs", "joaquin@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Andre Perro", "andre@gmail.com");
+
 		
 		//Ver o nome em persistence.xml
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
         //1) Paso: Salvar base de dados
+		//Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		//Pessoa p2 = new Pessoa(null, "Joaquin Balhs", "joaquin@gmail.com");
+		//Pessoa p3 = new Pessoa(null, "Andre Perro", "andre@gmail.com");
 		//em.getTransaction().begin();
 		//em.persist(p1);
 		//em.persist(p2);
@@ -28,9 +28,16 @@ public class Program {
 		
 		
 		//2) Paso: Procura uma pessoa por ID no DB
-		Pessoa p = em.find(Pessoa.class, 2); //no id: 2
-		System.out.println("Procuando pesso no id: 2 ");
-		System.out.println(p);
+		//Pessoa p = em.find(Pessoa.class, 2); //no id: 2
+		//System.out.println("Procuando pesso no id: 2 ");
+		//System.out.println(p);
+		
+		
+		//3) Paso: Removendo dados do DB: ERROR
+		//Pessoa p = new Pessoa(2, "Joaquin Balhs", "joaquin@gmail.com");
+		//em.remove(p); //para remover o objeto p deve estar como monitorado
+		//System.out.println("Pronto Removido");
+		//O que eh monitorado: Obj que se acabou do inserir ou que se procuro mas ainda nao feche o em.close()
 		
 		
 		em.close();
